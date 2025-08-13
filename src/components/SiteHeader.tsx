cb27e08b15bfa43c1dd1722225f5252f24f797f0
jl-max <1012634484@qq.com>
2025-08-13 21:16:16 +0800

    fix

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState, ReactNode } from "react";
+import { useState } from "react";
 
 interface NavItem {
   label: string;
