fa272d0cde2646232bf7a4a4b36217d1f2bcabdc
jl-max <1012634484@qq.com>
2025-08-13 21:13:50 +0800

    fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,8 +4,10 @@ const MONGODB_URI = process.env.MONGODB_URI!
 
 if (!MONGODB_URI) throw new Error('Missing MONGODB_URI')
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 let cached = (global as any).mongoose
 if (!cached) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   cached = (global as any).mongoose = { conn: null, promise: null }
 }
 
